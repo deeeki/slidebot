e39148d74b0e1f48a8878c3b5eeead429d241699
itzki <itzki.h@gmail.com>
2013-08-28 15:18:08 +0900

    Fix showing slide language

--- lib/slidebot/slideshare/tweetable.rb
+++ lib/slidebot/slideshare/tweetable.rb
@@ -8,14 +8,15 @@ module Slidebot
           prefix = "*#{prefix.to_s.capitalize}!* "
         end
         uploaded = Time.parse(created).strftime('%Y-%m-%d')
-        dl = download == '1' ? '[DL:OK]' : '[DL:NG]'
+        dl = (download == '1') ? '[DL:OK]' : '[DL:NG]'
+        lang = (language != '**') ? "[#{language}]" : ''
         hashtag = ::Hashtag.detect_array(tags.tag) if tags.is_a?(Hash)
         hashtag ||= ::Hashtag.detect(title)
         # https url is counted as 23 on Twitter
-        title_max_length = 140 - (prefix.to_s + username + uploaded + language + dl + hashtag.to_s).size - 12 - 23
+        title_max_length = 140 - (prefix.to_s + username + uploaded + lang + dl + hashtag.to_s).size - 10 - 23
         disp_title = title
         disp_title = title[0, title_max_length - 4] + ' ...' if title.size > title_max_length
-        "#{prefix}#{disp_title} #{url} (by #{username} #{uploaded}) [#{language}]#{dl} #{hashtag}"
+        "#{prefix}#{disp_title} #{url} (by #{username} #{uploaded}) #{lang}#{dl} #{hashtag}"
       end
     end
   end
